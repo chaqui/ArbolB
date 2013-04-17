8e11f677ff6e54cb5c8f2169676924c148846874
Josue <josue.chaqui@gmail.com>
2013-04-17 12:11:03 -0600

    iniciando el repo y creando la clase dato

---
+++ src/arbolb/Dato.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolb;
+
+/**
+ *
+ * @author chaqui
+ */
+public class Dato {
+    private int id;
+    private String modelo;
+    private String marca;
+    private int existencia;
+    private String producto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+    
+}
