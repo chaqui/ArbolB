307a5bdbba2987b2e1fb78364b2008577c311927
Josue <josue.chaqui@gmail.com>
2013-04-24 00:29:11 -0600

    iniciando ingreso de rama

--- src/arbolb/Pagina.java
+++ src/arbolb/Pagina.java
@@ -35,59 +35,150 @@ public class Pagina {
         }
     }
     private Pagina ingresarEnHoja(Dato datoInsertar,Pagina Padre){
-        if (this.elementos < this.max) {
-            if (this.hijos[1]!=null) {
-                this.datos[this.elementos]=datoInsertar;
-                boolean bandera=true;
-                while (bandera) {
-                    bandera=false;
-                    for (int i = 1; i < this.elementos; i++) {
-                        if (this.datos[i].getId() <this.datos[i-1].getId()) {
-                            Dato aux = this.datos[i];
-                            this.datos[i]=this.datos[i-1];
-                            this.datos[i-1]=aux;
-                            bandera=true;
+        boolean bandent=false;
+            //si no tiene hijo 1 es porque no tiene hijos
+                  bandent=(this.hijos[1]==null)?true:false;
+                  if (bandent) {
+                        // si no esta lleno es porque todavia podemos ingresar un valor
+                        if (this.elementos<this.max) {
+                            this.datos[this.elementos]= datoInsertar;
+                            boolean bandera=true;
+                            while (bandera) {
+                                bandera=false;
+                                for (int i = 1; i < this.elementos; i++) {
+                                    if (this.datos[i].getId() <this.datos[i-1].getId()) {
+                                        Dato aux = this.datos[i];
+                                        this.datos[i]=this.datos[i-1];
+                                        this.datos[i-1]=aux;
+                                        bandera=true;
+                                    }
+                                }
+                            }
+                            this.elementos++;
+                            return null;
                         }
+                         // si no es que esta llena la pagina y se debe de dividir
+                        else{
+                            Dato[] auxiliar = new Dato[max+1];
+                            for (int i = 0; i < max; i++) {
+                                auxiliar[i]= this.datos[i];
+                            }
+                            auxiliar[max]=datoInsertar;
+                            boolean bandera=true;
+                            while (bandera) {
+                                bandera=false;
+                                for (int i = 1; i <= max; i++) {
+                                    if (auxiliar[i].getId() <auxiliar[i-1].getId()) {
+                                        Dato aux = this.datos[i];
+                                        auxiliar[i]=this.datos[i-1];
+                                        auxiliar[i-1]=aux;
+                                        bandera=true;
+                                    }
+                                }
+                            }
+                            for (int i = 0; i < max; i++) {
+                                this.datos[i]=null;
+                            }
+                                this.datos[0]=auxiliar[(int)max%2+1];
+                                this.hijos[0]= new Pagina(max,this);
+                                for (int i = 0; i < (int)max%2+1; i++) {
+                                    hijos[0].insertar(auxiliar[i],this);
+                                    hijos[0].id=1;
+                                }
+                                this.hijos[1]= new Pagina(max,this);
+                                for (int i = 0; i < (int)max%2+1; i++) {
+                                    hijos[1].insertar(auxiliar[i],this);
+                                    hijos[1].id=2;
+                                }
+                           this.elementos=1;
+                            
+                        }  
                     }
-                }
-                this.elementos++;
-                return null;
-            }
-            else{
-                boolean baux=true;
-                for (int i = 0; i < this.elementos; i++) {
-                    if (datoInsertar.getId()< this.datos[i].getId()){
-                        if (this.hijos[i]!=null) {
-                            this.hijos[i].insertar(datoInsertar, this);
-                            baux=false;
+                  //si tiene hijos
+                    else{
+                        if (this.elementos<this.max) {
+                            boolean banIng=false;
+                            for (int i = 0; i < this.elementos; i++) {
+                                if (datoInsertar.getId()<this.datos[i].getId()) {
+                                    Pagina a=this.hijos[i].insertar(datoInsertar,this);
+                                     if (a!=null) {
+                                    this.actualizarPagina(a);
+                                    }
+                                    banIng=true;
+                                }
+                            }
+                            if (!banIng) {
+                                if (this.hijos[this.elementos]!=null) {
+                                      Pagina a=this.hijos[this.elementos].insertar(datoInsertar,this);
+                                     if (a!=null) {
+                                        this.actualizarPagina(a);
+                                    }
+                                }
+                                else{
+                                    if (this.datos[this.elementos]!=null) {
+                                        this.datos[this.elementos]=datoInsertar;
+                                        this.elementos++;
+                                        return null;
+                                    }
+                                }
+                            }
+                        } 
+                        else{
+                            boolean bank=true;   
+                            for (int i = 0; i < this.elementos; i++) {
+                                if (datoInsertar.getId()<this.datos[i].getId()) {
+                                    Pagina a=this.hijos[i].insertar(datoInsertar,this);
+                                     if (a!=null) {
+                                    this.actualizarPagina(a);
+                                    }
+                                    bank=false;
+                                }
+                            }
+                            if(bank){
+                                Pagina[] auxPag = new Pagina[max];
+                                Dato[] auxiliar = new Dato[max+1];
+                                for (int i = 0; i < max; i++) {
+                                auxiliar[i]= this.datos[i];
+                                }
+                                for (int i = 0; i < max; i++) {
+                                    auxPag= this.hijos;
+                                }
+                                auxiliar[max]=datoInsertar;
+                                boolean bandera=true;
+                                while (bandera) {
+                                    bandera=false;
+                                    for (int i = 1; i <= max; i++) {
+                                        if (auxiliar[i].getId() <auxiliar[i-1].getId()) {
+                                            Dato aux = auxiliar[i];
+                                            auxiliar[i]=auxiliar[i-1];
+                                            auxiliar[i-1]=aux;
+                                            if (auxPag[i]!=null && auxPag[i-1]!=null) {
+                                                Pagina auxP =  auxPag[i];
+                                                auxPag[i]=auxPag[i-1];
+                                                auxPag[i-1]=auxP; 
+                                                
+                                            }
+                                            bandera=true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                for (int i = 0; i < max; i++) {
+                                    this.datos[i]=null;
+                                }
+                                    this.datos[0]=auxiliar[(int)max%2+1];
+                                    this.hijos[0]= new Pagina(max,this);
+                                    for (int i = 0; i < (int)max%2+1; i++) {
+                                        hijos[0].insertar(auxiliar[i],this);
+                                    }
+                                    this.hijos[1]= new Pagina(max,this);
+                                    for (int i = 0; i < (int)max%2+1; i++) {
+                                        hijos[1].insertar(auxiliar[i],this);
+                                    }
+                                }
+                            }
                         }
-                    }
-                }
-                if (baux) {
-                    this.datos[this.elementos]=datoInsertar;
-                        boolean bandera=true;
-                              while (bandera) {
-                                  bandera=false;
-                                  for (int i = 1; i < this.elementos; i++) {
-                                      if (this.datos[i].getId() <this.datos[i-1].getId()) {
-                                          Dato aux = this.datos[i];
-                                          this.datos[i]=this.datos[i-1];
-                                          this.datos[i-1]=aux;
-                                          bandera=true;
-                                      }
-                                  }
-                              }
-                        this.elementos++;
                         return null;
-                }
-            }
-        }
-        else{
-            for (int i = 0; i < max; i++) {
-                
-            }
-        }
-        return null;
     }
     private Pagina ingresarEnRaiz(Dato datoInsertar,Pagina padre){
         boolean bandent=false;
